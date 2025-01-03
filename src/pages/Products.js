66fb1eabbc60cdcc16b010983726e6bcb76b8bbc
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-03 08:53:32 -0300

    feat: 346. Navigating between pages with links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function ProductsPage() {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <h1>Products Page</h1>
+      <p>
+        Back to the <Link to="/">home page</Link>
+      </p>
+    </>
+  );
 }
