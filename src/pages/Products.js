af32a97ee821ccfc5315f0f29425ab1dba47f5b9
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-03 10:14:30 -0300

    feat: 352. Adding linkes for dymanic routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 export default function ProductsPage() {
   return (
     <>
       <h1>Products Page</h1>
       <ul>
-        <li>
-          <Link to="/products/1">Product 1</Link>
-        </li>
-        <li>
-          <Link to="/products/2">Product 2</Link>
-        </li>
-        <li>
-          <Link to="/products/3">Product 3</Link>
-        </li>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
